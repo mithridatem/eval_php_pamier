9859224b9e9d7b8007e3e4a1f3cfa0d07a125ebc
mithridatem <mathieumithridate@adrar-formation.com>
2023-10-06 09:27:10 +0200

    Add fonction getArticle

--- result.php
+++ result.php
@@ -4,8 +4,16 @@
     if(isset($_POST['submit'])){
         //test si les champs sont tous remplis
         if(!empty($_POST['title'])AND !empty($_POST['content'])AND !empty($_POST['creation_date'])){
-            addArticle($bdd, $_POST['title'], $_POST['content'], $_POST['creation_date']);
-            $error = "L'article à été ajouté en BDD";
+            //tester si l'article existe 
+            if(!empty(getArticle($bdd, $_POST['title'],$_POST['content'], $_POST['creation_date']))){
+                $error = "L'article existe déja";
+            }
+            //tester si l'article n'existe pas
+            else{
+                //tester si l'article n'existe pas
+                addArticle($bdd, $_POST['title'], $_POST['content'], $_POST['creation_date']);
+                $error = "L'article à été ajouté en BDD";
+            }
         }
         //test les champs ne sont pas tous remplis
         else{
@@ -28,4 +36,18 @@
             die('Error : '.$e->getMessage());
         }
     }
-?>
+    function getArticle($bdd, $title, $content, $date){
+        try {
+            $req = $bdd->prepare("SELECT id, title, content, creation_date FROM article WHERE
+            title = ? AND content = ? AND creation_date = ?");
+            $req->bindParam(1, $title, PDO::PARAM_STR);
+            $req->bindParam(2, $content, PDO::PARAM_STR);
+            $req->bindParam(3, $date, PDO::PARAM_STR);
+            //exécution de la requête
+            $req->execute();
+            return $req->fetch(PDO::FETCH_ASSOC);
+        } 
+        catch (Exception $e) {
+            die('Error : '.$e->getMessage());
+        }
+    }
